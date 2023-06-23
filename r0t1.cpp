94afc27e7bcea764694990cb403574396618972d
Rohit Dhill <s4dr0t1@protonmail.com>
2023-06-23 08:00:00 +0530

    Working iteration of the driver

---
+++ r0t1.cpp
@@ -0,0 +1,35 @@
+#include <ntddk.h>
+
+
+void SampleUnload(
+	_In_ PDRIVER_OBJECT DriverObject
+);
+
+extern "C" NTSTATUS
+DriverEntry(
+	_In_ PDRIVER_OBJECT DriverObject,
+	_In_ PUNICODE_STRING RegistryPath
+) {
+
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+
+
+	DriverObject->DriverUnload = SampleUnload;
+
+	KdPrint(("My name is Sachin Kumar"));
+
+
+	return STATUS_SUCCESS;
+
+}
+
+
+void SampleUnload(
+	_In_ PDRIVER_OBJECT DriverObject
+) {
+	UNREFERENCED_PARAMETER(DriverObject);
+
+	KdPrint(("Driver unloaded"));
+}
